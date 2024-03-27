44b9ceb9c2530110ccc2d9f5070dd58e8ed2306e
Bertram Højlund Petersen <bepet22@student.sdu.dk>
2024-03-27 11:07:51 +0100

    More base functionality

--- BasicMap/src/main/java/dk/sdu/mmmi/BasicMapSystem/BasicMap.java
+++ BasicMap/src/main/java/dk/sdu/mmmi/BasicMapSystem/BasicMap.java
@@ -3,4 +3,7 @@ package dk.sdu.mmmi.BasicMapSystem;
 import dk.sdu.mmmi.common.data.Map;
 
 public class BasicMap extends Map {
+    public BasicMap(int width, int height) {
+        super(width, height);
+    }
 }
--- Common/src/main/java/dk/sdu/mmmi/common/data/Map.java
+++ Common/src/main/java/dk/sdu/mmmi/common/data/Map.java
@@ -1,4 +1,32 @@
 package dk.sdu.mmmi.common.data;
 
+import dk.sdu.mmmi.common.services.ICollidable;
+
 public class Map {
+
+    private ICollidable[][] map;
+    private int width;
+    private int height;
+
+    public Map(int width, int height) {
+        this.width = width;
+        this.height = height;
+        map = new ICollidable[width][height];
+    }
+
+    public ICollidable[][] getMap() {
+        return map;
+    }
+
+    public void setMap(ICollidable[][] map) {
+        this.map = map;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
