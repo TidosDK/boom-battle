3757b524beb2d6b77e03024dd980f7b774728e0b
Bertram Højlund Petersen <bepet22@student.sdu.dk>
2024-04-03 12:40:50 +0200

    Added the basic map generator
    It generates the default map as known from playing with fire with
    every other column having walls on every other row.

--- BasicMap/src/main/java/dk/sdu/mmmi/BasicMapSystem/BasicMapGenerator.java
+++ BasicMap/src/main/java/dk/sdu/mmmi/BasicMapSystem/BasicMapGenerator.java
@@ -6,7 +6,24 @@ import dk.sdu.mmmi.common.services.IMapGenerator;
 public class BasicMapGenerator implements IMapGenerator {
 
 
-    public void generateMap(World world) {
-        return;
+    public void generateMap(World world) {throw new UnsupportedOperationException("Not implemented yet");}
+
+    /**
+     * Generates a default map for the game where every other column has a wall on every other row
+     * @param width the width of the map
+     * @param height the height of the map
+     * @return a 2D boolean array representing the map
+     */
+    public boolean[][] basicMap(int width, int height) {
+        boolean[][] map = new boolean[width][height];
+        // Generate a map where every other column has a wall on every other row
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                map[x][y] = (x % 2 == 1) && (y % 2 == 1);
+            }
+        }
+        return map;
     }
 }
+
+
