cdf37dbc7e8567cd485e126864893b7dcf54edd4
Bertram Højlund Petersen <bepet22@student.sdu.dk>
2024-04-03 14:03:50 +0200

    Made world Singleton

--- Common/src/main/java/dk/sdu/mmmi/common/data/World.java
+++ Common/src/main/java/dk/sdu/mmmi/common/data/World.java
@@ -1,8 +1,18 @@
 package dk.sdu.mmmi.common.data;
 
 public class World {
+    private static World instance;
     private Map map;
 
+    private World() {}
+
+    public static World getInstance() {
+        if (instance == null) {
+            instance = new World();
+        }
+        return instance;
+    }
+    
     public Map getMap() {
         return map;
     }
