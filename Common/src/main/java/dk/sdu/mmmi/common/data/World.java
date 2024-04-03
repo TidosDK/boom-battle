85811b733c831c6f08fa74b9a45ef2f798c3e1f0
Bertram Højlund Petersen <bepet22@student.sdu.dk>
2024-04-03 12:52:00 +0200

    Added attributes to World class
    This can act as a placeholder until the GameEngine is fully implemented

--- Common/src/main/java/dk/sdu/mmmi/common/data/World.java
+++ Common/src/main/java/dk/sdu/mmmi/common/data/World.java
@@ -1,4 +1,13 @@
 package dk.sdu.mmmi.common.data;
 
 public class World {
+    private Map map;
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
 }
