89af1615c40c69ebe96b86b25a13e47311156608
Mathias Nicolajsen <28749155+TidosDK@users.noreply.github.com>
2024-04-07 02:39:29 +0200

    Renamed WeaponConstrolSystem

---
+++ Weapon/src/main/java/dk/sdu/mmmi/WeaponSystem/WeaponControlSystem.java
@@ -0,0 +1,38 @@
+package dk.sdu.mmmi.WeaponSystem;
+
+import dk.sdu.mmmi.common.data.Entity;
+import dk.sdu.mmmi.common.data.GameData;
+import dk.sdu.mmmi.common.data.World;
+import dk.sdu.mmmi.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.common.services.IWeapon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeaponControlSystem implements IEntityProcessingService, IWeapon {
+
+    private int damagePoints = 2;
+
+    @Override
+    public synchronized void process(World world, GameData gameData) {
+        for (Entity entity : world.getEntities(Weapon.class)) {
+            Weapon weapon = (Weapon) entity;
+            if (weapon.calculateTimeTillExplosion(gameData) <= 0) {
+                System.out.println("EXPLOSION");
+                // Calculate blast explosion
+                world.removeEntity(weapon);
+            }
+        }
+    }
+
+    @Override
+    public Entity createWeapon(Entity weaponPlacer, GameData gameData) {
+        Weapon weapon = new Weapon("Weapon/src/main/resources/weapon.png", 2f, 2f);
+        weapon.setX(weaponPlacer.getX());
+        weapon.setY(weaponPlacer.getY());
+        weapon.setDamagePoints(2);
+        weapon.setTimeSincePlacement(gameData.getDeltaTime());
+        weapon.setTimeTillExplosionInSeconds(2f);
+        return weapon;
+    }
+}
--- Weapon/src/main/java/module-info.java
+++ Weapon/src/main/java/module-info.java
@@ -1,3 +1,4 @@
+import dk.sdu.mmmi.WeaponSystem.WeaponControlSystem;
 import dk.sdu.mmmi.common.services.IWeapon;
 import dk.sdu.mmmi.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.common.services.IGamePluginService;
@@ -5,6 +6,6 @@ import dk.sdu.mmmi.common.services.IGamePluginService;
 module Weapon {
     requires Common;
     provides IGamePluginService with dk.sdu.mmmi.WeaponSystem.WeaponPlugin;
-    provides IWeapon with dk.sdu.mmmi.WeaponSystem.WeaponConstrolSystem;
-    provides IEntityProcessingService with dk.sdu.mmmi.WeaponSystem.WeaponConstrolSystem;
+    provides IWeapon with WeaponControlSystem;
+    provides IEntityProcessingService with WeaponControlSystem;
 }
