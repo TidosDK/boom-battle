6987599fa4df63249649865b683b6a538eb34d5d
Bertram Højlund Petersen <bepet22@student.sdu.dk>
2024-04-09 11:59:58 +0200

    Changed moving logic for Player

--- Player/src/main/java/dk/sdu/mmmi/player/PlayerControlSystem.java
+++ Player/src/main/java/dk/sdu/mmmi/player/PlayerControlSystem.java
@@ -7,6 +7,7 @@ import dk.sdu.mmmi.common.data.World;
 import dk.sdu.mmmi.common.services.IActor;
 import dk.sdu.mmmi.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.common.services.IWeapon;
+import dk.sdu.mmmi.common.services.Map.IMap;
 
 public class PlayerControlSystem implements IActor, IEntityProcessingService { // implements IDamageable
     private World world;
@@ -51,24 +52,26 @@ public class PlayerControlSystem implements IActor, IEntityProcessingService { /
 
     // @Override
     public void move(Direction direction) {
+        System.out.println("Moving player: x: %f, y: %f" + player.getX() + player.getY());
         player.setDirection(direction);
-
+        if (World.getInstance().getMap() instanceof IMap) {
+            IMap map = (IMap) World.getInstance().getMap();
+            if (!map.isMoveAllowed((int) player.getX(), (int) player.getY(), direction)) {
+                return;
+            }
+        }
         switch (direction) {
             case LEFT:
-                player.setX(player.getX() + (float) (MOVING_SPEED * Math.cos(Math.toRadians(player.getRotation() + 90)) * gameData.getDeltaTime()));
-                player.setY(player.getY() - (float) (MOVING_SPEED * Math.sin(Math.toRadians(player.getRotation() + 90)) * gameData.getDeltaTime()));
+                player.setX(player.getX() + (float) (MOVING_SPEED * -1 * gameData.getDeltaTime()));
                 break;
             case RIGHT:
-                player.setX(player.getX() - (float) (MOVING_SPEED * Math.cos(Math.toRadians(player.getRotation() - 90)) * gameData.getDeltaTime()));
-                player.setY(player.getY() + (float) (MOVING_SPEED * Math.sin(Math.toRadians(player.getRotation() - 90)) * gameData.getDeltaTime()));
+                player.setX(player.getY() + (float) (MOVING_SPEED * 1 * gameData.getDeltaTime()));
                 break;
             case UP:
-                player.setX(player.getX() + (float) (MOVING_SPEED * Math.cos(Math.toRadians(player.getRotation() + 90)) * gameData.getDeltaTime()));
-                player.setY(player.getY() + (float) (MOVING_SPEED * Math.sin(Math.toRadians(player.getRotation() + 90)) * gameData.getDeltaTime()));
+                player.setY(player.getY() + (float) (MOVING_SPEED * -1 * gameData.getDeltaTime()));
                 break;
-            case DOWN:
-                player.setX(player.getX() - (float) (MOVING_SPEED * Math.cos(Math.toRadians(player.getRotation() - 90)) * gameData.getDeltaTime()));
-                player.setY(player.getY() - (float) (MOVING_SPEED * Math.sin(Math.toRadians(player.getRotation() - 90)) * gameData.getDeltaTime()));
+            case DOWN:;
+                player.setY(player.getY() - (float) (MOVING_SPEED * 1 * gameData.getDeltaTime()));
                 break;
         }
     }
