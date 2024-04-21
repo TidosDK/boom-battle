d4a901c6ab02a33709782b43e3e305484382370c
phped22 <phped22@student.sdu.dk>
2024-04-22 01:57:29 +0200

    Added functionality to create explosion entities with relevant animations within the process function.

--- Weapon/src/main/java/dk/sdu/mmmi/WeaponSystem/WeaponControlSystem.java
+++ Weapon/src/main/java/dk/sdu/mmmi/WeaponSystem/WeaponControlSystem.java
@@ -16,15 +16,32 @@ public class WeaponControlSystem implements IEntityProcessingService, IWeaponCon
     public synchronized void process(World world, GameData gameData) {
         for (Entity entity : world.getEntities(Weapon.class)) {
             Weapon weapon = (Weapon) entity;
-            weapon.setTexturePath(weapon.getCurrentExplosionAnimatorPath());
             if (weapon.calculateTimeTillExplosion(gameData) <= 0) {
-                System.out.println("EXPLOSION");
-                // Calculate blast explosion
-                this.dealDamage(weapon.calculateBlastArea(world), world, weapon);
-                world.removeEntity(weapon);
+                // Explosion time reached; trigger the explosion visuals and effects.
+                    Collection<Coordinates> blastArea = weapon.calculateBlastArea(world);
+                    for (Coordinates coord : blastArea) {
+                        // For each coordinate in the blast area, create an explosion entity with the appropriate texture.
+                        String texturePath = weapon.getCurrentFireExplosionAnimatorPath(coord);
+                        System.out.println(coord.getX());
+                        System.out.println(coord.getY());
+                        Explosion explosion = new Explosion(texturePath, coord.getX(), coord.getY(), gameData.getScaler(), gameData.getScaler());
+                        System.out.println("Creating Explosion at: x=" + coord.getX() + ", y=" + coord.getY() + ", width=" + gameData.getScaler() + ", height=" + gameData.getScaler());
+
+                        world.addEntity(explosion);
+                    }
+                    // Damage calculation and handling.
+                    this.dealDamage(blastArea, world, weapon);
+                    // Remove the weapon entity after all explosion entities are placed.
+                    world.removeEntity(weapon);
+
+
+            } else {
+                // Set the texture path to the bomb as it counts down to explosion.
+                weapon.setTexturePath(weapon.getCurrentExplosionAnimatorPath());
             }
         }
     }
+
 
 
     private static void dealDamage(Collection<Coordinates> blastArea, World world, Weapon weapon) {
@@ -46,7 +63,7 @@ public class WeaponControlSystem implements IEntityProcessingService, IWeaponCon
         weapon.setX(weaponPlacer.getX());
         weapon.setY(weaponPlacer.getY());
         weapon.setDamagePoints(2);
-        weapon.setBlastLength(15);
+        weapon.setBlastLength(3);
         weapon.setTimeSincePlacement(gameData.getDeltaTime());
         weapon.setTimeTillExplosionInSeconds(2f);
         return weapon;
