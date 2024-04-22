5d3b4e4940900c7cf1cbec0ed0fbeb6565d0efa2
phped22 <phped22@student.sdu.dk>
2024-04-22 17:50:11 +0200

    Added time relevant attributes to explosion

--- Weapon/src/main/java/dk/sdu/mmmi/WeaponSystem/Explosion.java
+++ Weapon/src/main/java/dk/sdu/mmmi/WeaponSystem/Explosion.java
@@ -1,15 +1,35 @@
 package dk.sdu.mmmi.WeaponSystem;
 
 import dk.sdu.mmmi.common.data.Entity.Entity;
+import dk.sdu.mmmi.common.data.Properties.GameData;
 
 public class Explosion extends Entity {
-    public Explosion(String texturePath, float x, float y, float width, float height) {
+    private boolean finished;
+    private float elapsedTime;
+    private float animTime; // Duration of the explosion animation
+
+    public Explosion(GameData gameData, String texturePath, float x, float y, float width, float height, float animTime) {
         super(texturePath, width, height);
         this.setX(x); // Set the X coordinate
         this.setY(y); // Set the Y coordinate
+        this.animTime = animTime; // Set the duration of the animation
     }
+
+    public void setElapsedTime(float time) {
+        elapsedTime = time;
+    }
+
+    public float getElapsedTime() {
+        return elapsedTime;
+    }
+
+    public float getAnimTime() {
+        return animTime;
+    }
+
+
+
+
 }
-
-
 
 
