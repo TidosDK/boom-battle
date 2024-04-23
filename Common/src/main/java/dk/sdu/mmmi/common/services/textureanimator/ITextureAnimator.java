fefdada29492aa3cbb1b4dcab9de57d379ccc12e
MattisJensen <115666021+MattisJensen@users.noreply.github.com>
2024-04-23 19:49:48 +0200

    Added missing methods to IAnimatable

--- Common/src/main/java/dk/sdu/mmmi/common/services/textureanimator/ITextureAnimator.java
+++ Common/src/main/java/dk/sdu/mmmi/common/services/textureanimator/ITextureAnimator.java
@@ -8,4 +8,21 @@ public interface ITextureAnimator {
      * @return Current image path
      */
     Path getCurrentTexturePath();
+
+    /**
+     * @return returns the absolute amount of textures in the animation
+     */
+    int getTextureAmount();
+
+    /**
+     * @return returns the index of the current texture in the animation. Index starts at 0.
+     */
+    int getCurrentTextureIndex();
+
+    /**
+     * Sets the current texture index of the animation to the given index.
+     *
+     * @param index index to set the current texture index to
+     */
+    void setCurrentTextureIndex(int index);
 }
