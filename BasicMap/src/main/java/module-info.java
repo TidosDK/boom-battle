0ac9292f02cc6fea68a613eb47feba2659790d91
Bertram Højlund Petersen <bepet22@student.sdu.dk>
2024-04-24 10:23:30 +0200

    Map now renders obstacles and pathTiles

--- BasicMap/src/main/java/module-info.java
+++ BasicMap/src/main/java/module-info.java
@@ -2,6 +2,8 @@ import dk.sdu.mmmi.common.services.entityproperties.ICollidable;
 
 module BasicMap {
     uses ICollidable;
+    uses dk.sdu.mmmi.common.services.obstacle.nondestructible.INonDestructibleObstacleController;
+    uses dk.sdu.mmmi.common.services.obstacle.destructible.IDestructibleObstacleController;
     requires Common;
     provides dk.sdu.mmmi.common.services.map.IMap with dk.sdu.mmmi.basicmapsystem.BasicMap;
     provides dk.sdu.mmmi.common.services.map.IMapProcessingService with dk.sdu.mmmi.basicmapsystem.BasicMap;
--- DestructibleObstacle/src/main/java/module-info.java
+++ DestructibleObstacle/src/main/java/module-info.java
@@ -1,9 +1,11 @@
 import dk.sdu.mmmi.common.services.obstacle.destructible.IDestructibleObstacleController;
 import dk.sdu.mmmi.common.services.textureanimator.ITextureAnimator;
+import dk.sdu.mmmi.common.services.textureanimator.ITextureAnimatorController;
 import dk.sdu.mmmi.destructibleobstacle.DestructibleObstacleControlSystem;
 
 module DestructibleObstacle {
     requires Common;
     uses ITextureAnimator;
+    uses ITextureAnimatorController;
     provides IDestructibleObstacleController with DestructibleObstacleControlSystem;
 }
