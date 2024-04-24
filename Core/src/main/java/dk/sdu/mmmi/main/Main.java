00084a9af3336f4d61dfe6dc48d3752dacd1b38d
looseMole <62012079+looseMole@users.noreply.github.com>
2024-04-24 12:07:02 +0200

    Main import IActor again

--- Core/src/main/java/dk/sdu/mmmi/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/main/Main.java
@@ -16,6 +16,7 @@ import dk.sdu.mmmi.common.data.gameproperties.GameData;
 import dk.sdu.mmmi.common.data.world.World;
 import dk.sdu.mmmi.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.common.services.IGamePluginService;
+import dk.sdu.mmmi.common.services.entityproperties.IActor;
 import dk.sdu.mmmi.common.services.map.IMap;
 import dk.sdu.mmmi.common.services.map.IMapGenerator;
 
