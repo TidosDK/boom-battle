738aa004116f5bb91416a32fa6d016bcbb59d630
Emil Jørgensen <1709emil@gmail.com>
2024-04-28 16:25:46 +0200

    added . to end of java doc

--- Common/src/main/java/dk/sdu/mmmi/common/services/ai/IOptimalBombPlacement.java
+++ Common/src/main/java/dk/sdu/mmmi/common/services/ai/IOptimalBombPlacement.java
@@ -1,7 +1,7 @@
 package dk.sdu.mmmi.common.services.ai;
 
 /**
- * Interface to be used for optimal bomb placement
+ * Interface to be used for optimal bomb placement.
  * This interface is used to find the optimal placement for a bomb
  */
 public interface IOptimalBombPlacement {
--- Common/src/main/java/dk/sdu/mmmi/common/services/ai/IPathFinding.java
+++ Common/src/main/java/dk/sdu/mmmi/common/services/ai/IPathFinding.java
@@ -5,7 +5,7 @@ import dk.sdu.mmmi.common.data.world.Map;
 
 import java.util.ArrayList;
 /**
- * Interface to be used for pathfinding
+ * Interface to be used for pathfinding.
  * This interface is used to find a path from a start node to a goal node
  * @see dk.sdu.mmmi.common.data.ai.Node
  */
