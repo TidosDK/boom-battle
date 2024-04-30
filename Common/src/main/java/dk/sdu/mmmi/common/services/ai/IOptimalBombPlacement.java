58988da53cf420be8994190ba3265370898f3377
Emil Jørgensen <1709emil@gmail.com>
2024-04-30 05:49:44 +0200

    Linting error

--- Common/src/main/java/dk/sdu/mmmi/common/services/ai/IOptimalBombPlacement.java
+++ Common/src/main/java/dk/sdu/mmmi/common/services/ai/IOptimalBombPlacement.java
@@ -7,5 +7,5 @@ import dk.sdu.mmmi.common.data.world.Map;
  * This interface is used to find the optimal placement for a bomb.
  */
 public interface IOptimalBombPlacement {
-    Node optimalBombPlacement(Node currentNode,Node goalNode, Map map);
+    Node optimalBombPlacement(Node currentNode, Node goalNode, Map map);
 }
